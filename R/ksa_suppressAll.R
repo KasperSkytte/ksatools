b1364760e305811fc77aec95794ab6b6d8ba5a7f
KasperSkytte <knaldhat@gmail.com>
2018-01-15 10:27:52 +0100

    Added ksa_suppressAll
    Suppresses all messages/warnings/errors/prints

---
+++ R/ksa_suppressAll.R
@@ -0,0 +1,20 @@
+#' @title Suppress all types of diagnostic messages, error messages, and/or outputs
+#' @description Simply wraps the given expression in \code{\link{tryCatch}}, \code{\link{invisible}}, \code{\link{capture.output}}, \code{\link{suppressMessages}}, \code{\link{suppressWarnings}}, and \code{\link{suppressPackageStartupMessages}} to suppress any possible console output.
+#'
+#' @param expr Expression to be evaluated.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' ksa_suppressAll({
+#'   stop("Some error")
+#'   print("Some print")
+#'   message("Some message")
+#'   some_variable <- "test"
+#'   return(some_variable)
+#'   })
+#'
+ksa_suppressAll <- function(expr) {
+  tryCatch({invisible(capture.output(suppressMessages(suppressWarnings(suppressPackageStartupMessages(expr)))))}, error = function(e) invisible())
+}
