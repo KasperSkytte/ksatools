865864833398fcc63ca5614c7eff1d682dc24a60
KasperSkytte <knaldhat@gmail.com>
2018-02-08 18:42:04 +0100

    can now also SET column classes... awesome

--- R/ksa_colClasses.R
+++ R/ksa_colClasses.R
@@ -1,15 +1,42 @@
-#' @title Column classes of a data frame
-#' @description Returns a vector with the column classes of a data frame like object.
+#' @title Get or set the column classes of a dataframe
+#' @description Returns a vector with the column classes of a dataframe like object, or coerces the column classes as defined by user.
 #'
-#' @param df A data frame like object
+#' @param df Dataframe like object
+#' @param value Character vector containing elements of: \code{"c"} (character), \code{"n"} (numeric), \code{"f"} (factor), \code{"l"} (logical)" to set the column classes of \code{df}.
 #' @note Cannot handle columns with more than one class.
-#' @return A data frame containing the column classes of the provided data frame.
+#' @return A dataframe containing the column classes of the provided dataframe, or a dataframe with coerced columns
 #' @export
 #'
 #' @examples
-#' ksa_colClasses(iris)
+#' df <- iris[1:10,]
+#' ksa_colClasses(df)
+#' ksa_colClasses(df) <- "nncfc"
+#' ksa_colClasses(df)
 
 ksa_colClasses <- function(df) {
   class <- unlist(lapply(unclass(df), class))
   return(as.data.frame(class))
+}
+
+`ksa_colClasses<-` <- function(df, value) {
+  if(nchar(value) != ncol(df))
+    stop("The number of columns in the dataframe does not match the number of characters in the vector of classes.")
+  if(!is.character(value))
+    stop("The classes must be provided as a character vector containing elements of: \"c\" (character), \"n\" (numeric), \"f\" (factor), \"l\" (logical)")
+  value <- unlist(strsplit(value, ""))
+  allowedClasses <- c("c", "n", "f", "l")
+  for(i in 1:ncol(df)) {
+    if(!any(value[[i]] %in% allowedClasses))
+      stop(paste0("Unknown class \"", value[[i]], "\". Allowed classes are: \"c\", \"n\", \"f\", or \"l\""))
+    if(value[[i]] == "c") {
+      df[[i]] <- as.character(df[[i]])
+    } else if(value[[i]] == "n") {
+      df[[i]] <- as.numeric(df[[i]])
+    } else if(value[[i]] == "f") {
+      df[[i]] <- as.factor(df[[i]])
+    } else if(value[[i]] == "l") {
+      df[[i]] <- as.logical(df[[i]])
+    }
   }
+  return(df)
+}
