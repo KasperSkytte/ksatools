048ef2b1967dacca85668ff2e4213c239964f987
KasperSkytte <knaldhat@gmail.com>
2018-02-08 18:42:26 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ R/ksa_odd.even_number.R
@@ -0,0 +1,19 @@
+#' @title Even number?
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ksa_is_even <- function(x) {x %% 2 == 0}
+
+#' @title Odd number?
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ksa_is_odd <- function(x) {x %% 2 != 0}
