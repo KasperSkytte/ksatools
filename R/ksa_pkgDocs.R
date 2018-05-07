9a33b2d19115884522417097eb55eb3075c45acb
KasperSkytte <knaldhat@gmail.com>
2018-05-07 14:58:00 +0200

    added build() and install() to ksa_pkgdocs()

--- R/ksa_pkgDocs.R
+++ R/ksa_pkgDocs.R
@@ -1,12 +1,15 @@
-#' @title Generate package documentation and webpage
+#' @title Build, install, and roxygenize() a package, then generate webpage
 #' @description Just runs \code{roxygen2::roxygenize(); pkgdown::clean_site(); pkgdown::build_site()}. I ran this so many times now that a small function like this is actually useful.
 #'
 #' @importFrom roxygen2 roxygenize
 #' @importFrom pkgdown clean_site build_site
+#' @importFrom devtools build install
 #' @export
 
 ksa_pkgDocs <- function() {
   roxygen2::roxygenize()
+  devtools::build()
+  devtools::install()
   pkgdown::clean_site()
   pkgdown::build_site()
 }
