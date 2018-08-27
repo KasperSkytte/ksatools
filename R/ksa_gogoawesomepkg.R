571f8bc149b2bfc4b5bfecc386ae1311ffb66ccb
KasperSkytte <knaldhat@gmail.com>
2018-08-27 18:02:50 +0200

    made devtools::check() optional

--- R/ksa_gogoawesomepkg.R
+++ R/ksa_gogoawesomepkg.R
@@ -1,16 +1,18 @@
 #' @title Document, check, and install a package
 #' @description Everything I do before commiting changes to GitHub. Generates package man pages, checks, installs, and builds package documentation website etc.
 #'
-#' @param install Install the package (\code{TRUE}) with \code{\link[devtools]{install}} or just load it in memory (\code{FALSE}) with \code{\link[devtools]{load_all}}. (Default: \code{TRUE})
+#' @param install Install the package (\code{TRUE}) using \code{\link[devtools]{install}} or just load it in memory (\code{FALSE}) using \code{\link[devtools]{load_all}}. (Default: \code{TRUE})
 #' @param pkgdown Build package documentation website or not. (Default: \code{TRUE})
 #' @param verbose Print status messages during the process or not. (Default: \code{TRUE})
+#' @param check Check the package using \code{\link[devtools]{check}} or not. (Default: \code{TRUE})
 #'
 #' @importFrom pkgdown clean_site build_site
 #' @importFrom devtools build document install check load_all
 #' @importFrom cli cat_line cat_rule cat_boxx
 #' @export
 #' @return Invisibly returns a list with the output of all steps run.
-ksa_gogoawesomepkg <- function(install = TRUE,
+ksa_gogoawesomepkg <- function(check = FALSE,
+                               install = TRUE,
                                pkgdown = TRUE,
                                verbose = TRUE) {
   outlist <- list()
@@ -20,9 +22,11 @@ ksa_gogoawesomepkg <- function(install = TRUE,
   outlist$devtools_document <- devtools::document()
 
   #R CMD CHECK
-  if(isTRUE(verbose))
-    cli::cat_rule(center = "Building and checking package")
-  outlist$devtools_check <- devtools::check(document = FALSE)
+  if(isTRUE(check)) {
+    if(isTRUE(verbose))
+      cli::cat_rule(center = "Building and checking package")
+    outlist$devtools_check <- devtools::check(document = FALSE)
+  }
 
   #install or just load
   if(isTRUE(install)) {
