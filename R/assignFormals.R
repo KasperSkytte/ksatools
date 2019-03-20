dd9c4f45b7d79d2dfbac724757c6fb5939614e26
Kasper Skytte Andersen <knaldhat@gmail.com>
2019-03-20 16:52:18 +0100

    assignFormals: removed status messages, missing arguments cause trouble

--- R/assignFormals.R
+++ R/assignFormals.R
@@ -6,56 +6,22 @@
 #' @param fun Function from which to extract defaults
 #' @param envir Environment in which to assign the default values of the arguments
 #'
-#' @return Invisibly returns a list with 3 elements; The function formals, assigned formals, and unassigned formals
+#' @return Nothing (\code{invisible()}). The function is only used for its side-effects.
 #' @export
 #' @author Kasper Skytte Andersen
 assignFormals <- function(fun, envir = .GlobalEnv) {
-  formals <- unlist(formals(fun))
+  if (!is.function(fun)) {
+    stop("Argument must be a function or the name of a function in quotes", call. = FALSE)
+  }
+  formals <- formals(fun)
   if (is.null(formals)) {
     stop("Function has no formals", call. = FALSE)
   }
-  noDefaults <- character()
-  assignedFormals <- character()
   for (i in seq_along(formals)) {
-    if (formals[[i]] != "") {
-      assign(names(formals[i]),
-        formals[[i]],
-        envir = envir
-      )
-      assignedFormals <- c(assignedFormals, names(formals[i]))
-    } else {
-      noDefaults <- c(noDefaults, names(formals[i]))
-    }
+    assign(names(formals[i]),
+      formals[[i]],
+      envir = envir
+    )
   }
-  message(paste0(
-    "* ",
-    length(assignedFormals),
-    " formal",
-    if (length(assignedFormals) == 1) {
-      " was"
-    } else {
-      "s were"
-    },
-    " assigned in the environment \"",
-    environmentName(envir),
-    "\":\n",
-    paste0(assignedFormals, collapse = ", ")
-  ))
-  if (length(noDefaults) > 0) {
-    message(paste0(
-      "* ",
-      length(noDefaults),
-      " formal",
-      if (length(noDefaults) > 1) {
-        "s"
-      },
-      " had no default:\n",
-      paste0(noDefaults, collapse = ", ")
-    ))
-  }
-  invisible(list(
-    formals = formals,
-    assignedFormals = assignedFormals,
-    noDefaults = noDefaults
-  ))
+  invisible()
 }
