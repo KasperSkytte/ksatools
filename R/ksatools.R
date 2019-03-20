fcad54f7132c438271da9744b5d2f6d9155eac7a
Kasper Skytte Andersen <knaldhat@gmail.com>
2019-03-20 16:52:46 +0100

    added package level documentation (and global vars)

---
+++ R/ksatools.R
@@ -0,0 +1,10 @@
+#' @title \code{ksatools} package
+#' @description Tools to make my R and package development life easier.
+#' @details Please use the online documentation webpage at \href{https://kasperskytte.github.io/ksatools/}{https://kasperskytte.github.io/ksatools/}
+#' @docType package
+#' @name ksatools
+NULL
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c(".", ".rs.restartR", "i"))
+}
